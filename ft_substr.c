c968c32594ba884608f7fe6084e205a693f60ad6
Benjamin Perez <bperez@z1r1p5.le-101.fr>
2019-11-18 15:31:45 +0100

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,23 +6,23 @@
 /*   By: bperez <marvin@le-101.fr>                  +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/09 17:06:34 by bperez       #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/07 19:42:11 by bperez      ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/18 15:31:19 by bperez      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include <string.h>
 #include "libft.h"
+
+#include <string.h>
 
 char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
 	char	*s2;
 
-	s2 = NULL;
-	if (s && (s2 = (char *)ft_calloc(len + 1, sizeof(char))))
+	if ((s2 = (char *)ft_calloc(len + 1, sizeof(char))) && start < ft_strlen(s))
 	{
 		s += start;
-		while (len--)
+		while (len-- && s[len])
 			s2[len] = s[len];
 	}
 	return (s2);
